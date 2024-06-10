04eb81f98454ca73c08f67dee7d0a51445b64a7e
dominikaglow <dominika01.glow@gmail.com>
2024-06-10 09:25:32 +0200

    Added stylesheets

--- client/src/components/commentComponent/commentSection.js
+++ client/src/components/commentComponent/commentSection.js
@@ -1,6 +1,7 @@
 // CommentSection.js
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "../../stylesheets/CommentSection.css";
 
 const CommentSection = ({ chapterId }) => {
     const [comments, setComments] = useState([]);
