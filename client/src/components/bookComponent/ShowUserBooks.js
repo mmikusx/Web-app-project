971078948d0cc886869156d23fc97df2e6f77f5b
Jakub Samel <kubaq215@interia.pl>
2024-06-27 17:05:10 +0200

    create different css file for UserBooks

--- client/src/components/bookComponent/ShowUserBooks.js
+++ client/src/components/bookComponent/ShowUserBooks.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import '../../stylesheets/BookById.css';
+import '../../stylesheets/UserBooks.css';
 import AddBook from './AddBook';
 import AddChapter from './AddChapter';
 
